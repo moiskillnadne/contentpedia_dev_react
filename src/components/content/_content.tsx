6e7d6374ebcb94ef2181721d555066b6aed18688
victor ryabkov <victor.ryabkov.business@gmail.com>
2020-11-18 19:48:04 +0300

    bug: fix import path

--- src/components/content/_content.tsx
+++ src/components/content/_content.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import GetColumn from '@/components/content/get/getColumn'
-import AddColumn from '@/components/content/add'
+import AddColumn from '@/components/content/add/_addColumn'
 import UpdateColumn from '@/components/content/update/updateColumn'
 import DeleteColumn from '@/components/content/remove/deleteColumn'
 
