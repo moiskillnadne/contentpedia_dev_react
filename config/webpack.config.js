43055f73994ab40770264aa3aeb9e520da3208c7
Victor Ryabkov <victorrabkov@Victors-MacBook-Air.local>
2020-12-31 20:20:53 +0300

    fix: type checking

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -620,21 +620,21 @@ module.exports = function(webpackEnv) {
             // Generate a service worker script that will precache, and keep up to date,
             // the HTML & assets that are part of the webpack build.
             isEnvProduction &&
-            // new WorkboxWebpackPlugin.GenerateSW({
-            //     clientsClaim: true,
-            //     exclude: [/\.map$/, /asset-manifest\.json$/],
-            //     importWorkboxFrom: 'cdn',
-            //     navigateFallback: paths.publicUrlOrPath + 'index.html',
-            //     navigateFallbackBlacklist: [
-            //         // Exclude URLs starting with /_, as they're likely an API call
-            //         new RegExp('^/_'),
-            //         // Exclude any URLs whose last part seems to be a file extension
-            //         // as they're likely a resource and not a SPA route.
-            //         // URLs containing a "?" character won't be blacklisted as they're likely
-            //         // a route with query params (e.g. auth callbacks).
-            //         new RegExp('/[^/?]+\\.[^/]+$'),
-            //     ],
-            // }),
+            new WorkboxWebpackPlugin.GenerateSW({
+                clientsClaim: true,
+                exclude: [/\.map$/, /asset-manifest\.json$/],
+                importWorkboxFrom: 'cdn',
+                navigateFallback: paths.publicUrlOrPath + 'index.html',
+                navigateFallbackBlacklist: [
+                    // Exclude URLs starting with /_, as they're likely an API call
+                    new RegExp('^/_'),
+                    // Exclude any URLs whose last part seems to be a file extension
+                    // as they're likely a resource and not a SPA route.
+                    // URLs containing a "?" character won't be blacklisted as they're likely
+                    // a route with query params (e.g. auth callbacks).
+                    new RegExp('/[^/?]+\\.[^/]+$'),
+                ],
+            }),
             // TypeScript type checking
             useTypeScript &&
             new ForkTsCheckerWebpackPlugin({
